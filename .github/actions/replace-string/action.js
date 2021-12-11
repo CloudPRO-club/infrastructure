169ce1efc29554b00634db20cc2d9cce5cfe2663
Anton Boyko <me@boykoant.pro>
2021-12-11 02:01:28 +0200

    continued with infra automatization for wiki

--- .github/actions/replace-string/action.js
+++ .github/actions/replace-string/action.js
@@ -2,8 +2,12 @@ const core = require('@actions/core');
 
 try {
     const sourceString = core.getInput('source-string');
-    const oldSubstring = core.getInput('old-substring');
+    let oldSubstring = core.getInput('old-substring');
     const newSubstring = core.getInput('new-substring');
+
+    if (!oldSubstring) {
+        oldSubstring = " ";
+    }
 
     const resultString = sourceString.split(oldSubstring).join(newSubstring);
 
